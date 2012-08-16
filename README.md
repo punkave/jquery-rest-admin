cca8e2b9c520a1d7653a7d37017c63b3eb4fae81
Tom Boutell <tom@punkave.com>
2012-08-16 19:25:44 -0400

    First incredibly rough stab at documentation; got it into a repository

--- README.md
+++ README.md
@@ -2,3 +2,86 @@ jquery-rest-admin
 =================
 
 A jQuery plugin providing an admin panel for any REST backend. Supports nested panels.
+
+Here's an example with text, checkbox and single select controls. Note that we pass in an array of JSONified "type" objects as the choices for one of the single select dropdowns. We can do that, as long as we specify what the choiceLabel and choiceValue property names are:
+
+    $(function() {
+      var types = [ { name: "Free", id: 1 }, { name: "Jobs", id: 2 } ];
+      $('.categories').restAdmin({
+        url: '/admin/categories',
+        schema: [ 
+          { 
+            name: "name", 
+            type: "text", 
+            label: "Name",
+            unique: true,
+            required: true
+          },
+          { 
+            name: "posters",
+            type: "select",
+            label: "Who Can Post?",
+            choices: [ { label: "Everyone", value: "everyone" }, { label: "Admins", value: "admins" }],
+            required: true
+          },
+          { 
+            name: "type",
+            type: "select",
+            label: "Type",
+            choices: types,
+            choiceLabel: 'name',
+            choiceValue: 'id',
+            required: true
+          },
+          {
+            name: "paid_option",
+            type: "checkbox",
+            label: "Paid"
+          }
+        ] 
+      });
+    });
+
+Here's another example with another admin nested in the first, allowing every department to have an editable list of aliases:
+
+    $(function() {
+      // Preloading the data is optional, it'll go get it via REST if you don't
+      var items = [ { "name": "Biology", "id": 1 }, "name": "Chemistry", "id" 2 } ];
+      $('.departments').restAdmin({
+        data: items, 
+        url: '/admin/departments', 
+        sortable: true,
+        container: '.departments', 
+        schema: [ 
+          { 
+            name: "name", 
+            type: "text", 
+            label: "Name",
+            required: true,
+            unique: true
+          },
+          {
+            name: "aliases",
+            type: "admin",
+            label: "Aliases",
+            labelColumn: "name",
+            options: {
+              sortable: false,
+              schema: [
+                {
+                  name: "name",
+                  type: "text",
+                  label: "Name",
+                  required: true,
+                  unique: true
+                }
+              ]
+            }
+          }
+        ] 
+      });
+    });
+
+Note that by default nested admins don't do their own REST requests. Instead the entire thing is serialized as part of the POST or PUT request for the parent object.
+
+You can override LOTS of things. There are lots of things you can't override yet. TODO: document all that; implement more great stuff; present references for REST and an example REST controller that this plugin can talk to (it really doesn't matter though, if it speaks REST properly and responds with JSON).
---
+++ web/js/jquery.restAdmin.js
@@ -0,0 +1,547 @@
+// REST-powered admin for any table of data.
+//
+// var items = [ { name: 'foo' }, { name: 'baz'} ];
+// aRestAdmin({ 
+//   data: items, 
+//   url: '/admin/products',
+//   sortable: true, 
+//   container: '.types', 
+//   schema: [ 
+//     { 
+//       name: "name", 
+//       type: "text", 
+//       label: "Name" 
+//     } 
+//   ] 
+// });
+//
+// There are additional options as seen below.
+
+(function( $ ){
+
+  $.fn.restAdmin = function(options) {
+  
+    var container = this;
+
+    // Primary key column name. There must be one to save and delete items.
+    // It must be acceptable to append it to the URL like this: /123
+    //
+    // It must never conflict with reasonable action keywords like: /rank
+    // (translation: slugs are inappropriate). You can get around this rule
+    // if you override REST actions that add a keyword to the URL (see below)
+
+    var idColumn = 'id';
+    if (options.id)
+    {
+      idColumn = options.id;
+    }
+
+    // Used to assign temporary IDs to new objects. Needed
+    // only when options.local is true (typically for nested admins).
+    // The temporary IDs start with _temp_ so they are easier to spot 
+    // server-side if necessary.
+    var tempId = 1;
+
+    // You can override each REST action. You can also set options.local = true;
+    // to just update options.data rather than carrying out REST actions. This is
+    // very useful for sub-admins
+
+    _.defaults(options, { 'local': false });
+
+    if (options.local)
+    {
+      _.defaults(options, {
+        create: function(datum, params)
+        {
+          // We need IDs to distinguish objects even for purely local data.
+          datum[idColumn] = '_temp_' + tempId++;
+          options.data.push(datum);
+          params.success(datum);
+        },
+        update: function(datum, params)
+        {
+          // datum is already a reference to an
+          // existing object in the list
+          params.success(datum);
+        },
+        refreshData: function(params)
+        {
+          // We hold the data so we don't need to refresh anything.
+          // We do need to do the initialization though
+          if (options.data === undefined)
+          {
+            options.data = [];
+          }
+          params.success();
+        },
+        remove: function(datum, params)
+        {
+          // Remove the unwanted element while ensuring that 'data'
+          // remains a reference to the same array object, which is a
+          // critical issue for local storage
+
+          var i;
+          var index = false;
+          for (i = 0; (i < options.data.length); i++)
+          {
+            var item = options.data[i];
+            if (item[idColumn] === datum[idColumn])
+            {
+              index = i;
+              break;
+            }
+          }
+          if (index !== false)
+          {
+            for (i = index + 1; (i < options.data.length); i++)
+            {
+              options.data[i - 1] = options.data[i];
+            }
+            options.data.length--;
+          }
+          params.success(datum);
+        },
+        rank: function(order, params)
+        {
+          var byId = _.groupBy(options.data, function(datum) {
+            return datum[idColumn];
+          });
+          // Rebuild the array without losing our reference
+          // to the same object, which is a critical issue for local storage
+          options.data.length = 0;
+          _.each(order, function(id) {
+            options.data.push(byId[id]);
+          })
+          params.success(order);
+        }
+      });
+    }
+
+    // Standard REST functionality
+
+    // POST a new item
+    if (!options.create)
+    {
+      options.create = function(datum, params)
+      {
+        $.ajax(options.url, { 
+          type: 'POST', 
+          data: datum,
+          dataType: 'json',
+          success: params.success,
+          error: params.error
+        });
+      }
+    }
+
+    // PUT an existing item
+    if (!options.update)
+    {
+      options.update = function(datum, params)
+      {
+        $.ajax(options.url + '/' + datum[idColumn], { 
+          type: 'PUT', 
+          data: datum,
+          dataType: 'json',
+          success: params.success,
+          error: params.error
+        });
+      }
+    }
+
+    // GET all items
+    if (!options.loadData)
+    {
+      options.loadData = function(params)
+      {
+        $.ajax(options.url, { 
+          type: 'GET', 
+          dataType: 'json',
+          success: params.success,
+          error: params.error
+        });
+      }
+    }
+
+    // DELETE an existing item
+    if (!options.remove)
+    {
+      options.remove = function(datum, params)
+      {
+        $.ajax(options.url + '/' + datum[idColumn], { 
+          type: 'DELETE', 
+          dataType: 'json',
+          success: params.success,
+          error: params.error
+        });
+      }
+    }
+
+    // Change sort order for all items
+    // (never called if options.sortable is false)
+    if (!options.rank)
+    {
+      options.rank = function(order, params)
+      {
+        $.ajax(options.url + '/rank', { 
+          type: 'PUT', 
+          dataType: 'json',
+          data: { order: order },
+          success: params.success,
+          error: params.error
+        });
+      }
+    }
+
+    // Types can be overridden freely to any degree
+
+    if (!options.types)
+    {
+      options.types = {};
+    }
+
+    if (!options.types.text)
+    {
+      options.types.text = {};
+    }
+
+    _.defaults(options.types.text, {
+      listText: function(column, val) {
+        return val;
+      },
+      control: function(column, val) {
+        var e = $('<input type="text" data-role="control" />');
+        e.val(val);
+        return e;
+      },
+      defaultValue: ''
+    });
+
+    if (!options.types.checkbox)
+    {
+      options.types.checkbox = {};
+    }
+
+    _.defaults(options.types.checkbox, {
+      listText: function(column, val) {
+        return val ? 'Yes' : 'No'
+      },
+      control: function(column, val) {
+        var e = $('<input type="checkbox" data-role="control" value="1" />');
+        e[0].checked = val;
+        return e;
+      },
+      defaultValue: false
+    });
+
+    if (!options.types.select)
+    {
+      options.types.select = {};
+    }
+
+    _.defaults(options.types.select, {
+      listText: function(column, val) {
+        var labelColumn = column.choiceLabel ? column.choiceLabel : 'label';
+        var valueColumn = column.choiceValue ? column.choiceValue : 'value';
+        var choice = _.find(column.choices, function(choice) {
+          // Allows both simple values and nested objects that have a suitable value column
+          if (typeof(val) !== 'object')
+          {
+            return (choice[valueColumn] === val);
+          }
+          else
+          {
+            return (choice[valueColumn] === val[valueColumn]);
+          }
+        });
+        return choice ? choice[labelColumn] : null;
+      },
+      control: function(column, val) {
+        var labelColumn = column.choiceLabel ? column.choiceLabel : 'label';
+        var valueColumn = column.choiceValue ? column.choiceValue : 'value';
+        var e = $('<select data-role="control" />');
+        _.each(column.choices, function(choice) {
+          var option = $('<option></option>');
+          option.text(choice[labelColumn]);
+          option.attr('value', choice[valueColumn]);
+          e.append(option);
+        });
+        // Allows both simple values and nested objects that have a suitable value column
+        if (typeof(val) === 'object')
+        {
+          e.val(val[valueColumn]);
+        }
+        else
+        {
+          e.val(val);
+        }
+        return e;
+      },
+      defaultValue: undefined
+    });
+
+    // Want to nest an admin in an admin? Who am I to say no?
+
+    if (!options.types.admin)
+    {
+      options.types.admin = {};
+    }
+
+    _.defaults(options.types.admin, {
+      listText: function(column, val) {
+        var labels = [];
+        _.each(val, function(datum) {
+          labels.push(datum[column.labelColumn]);
+        });
+        return labels.join(', ');
+      },
+      control: function(column, val) {
+        var e = $('<div class="nested-rest-admin" data-role="nestedRestAdmin"></div>');
+        var subAdminOptions = {};
+        _.defaults(column.options, { local: true });
+        _.extend(subAdminOptions, column.options, { data: val });
+        e.restAdmin(subAdminOptions);
+        return e;
+      },
+      // Don't call val(), we update directly by reference
+      selfUpdating: true,
+      // We need an array object at a minimum so we can modify it by reference and
+      // see that from the parent object when using 'local' storage
+      defaultValue: []
+    });
+
+    if (!options.refreshData)
+    {
+      options.refreshData = function(params)
+      {
+        options.loadData({
+          success: function(newData) {
+            options.data = newData;
+            params.success();
+          }
+        });
+      }
+    }
+
+    if (!options.data)
+    {
+      options.refreshData({ success: list });
+    }
+    else
+    {
+      list();
+    }
+
+    function eachColumn(columnFunction)
+    {
+      _.each(options.schema, function(column) {
+        columnFunction(column);
+      });
+    }
+
+    function list()
+    {
+      var table = $('<table></table>');
+      var headerRow = $('<tr></tr>');
+
+      eachColumn(function(column) {
+        var th = $('<th></th>');
+        th.text(column.label);
+        headerRow.append(th);
+      });
+      headerRow.append($('<th>Actions</th>'));
+      var header = $('<thead></thead>');
+      header.append(headerRow);
+      table.append(header);
+      var tbody = $('<tbody></tbody>');
+      _.each(options.data, function(datum) {
+        var row = $('<tr data-role="row"></tr>');
+        var first = true;
+        eachColumn(function(column) {
+          var val = options.types[column.type].listText(column, datum[column.name]);
+          var td = $('<td></td>');
+          if (first)
+          {
+            if (options.sortable)
+            {
+              td.append($('<i class="icon-move"></i>'));
+            }
+            first = false;
+          }
+          var text = $('<span><span>').text(options.types[column.type].listText(column, datum[column.name]));
+          td.append(text);
+          row.append(td);
+        });
+        var controls = $('<td><a href="#" class="btn" data-role="edit">Edit</a></td>');
+        row.append(controls);
+        row.data('id', datum[idColumn]);
+        tbody.append(row);
+        row.css('cursor', 'pointer');
+        controls.find('[data-role="edit"]').click(function() {
+          edit(datum);
+          return false;
+        });
+      });
+      if (options.sortable)
+      {
+        tbody.sortable().disableSelection().bind('sortupdate', function(event, ui) {
+          var items = tbody.find('[data-role="row"]');
+          rankMap = [];
+          items.each(function(i, item) {
+            rankMap.push($(item).data('id'));
+          });
+          options.rank(rankMap, {
+            error: function() {
+              // If something goes wrong server-side,
+              // we should no longer guess at the appropriate
+              // sort order. Just refresh. Om.
+              options.refreshData({ success: list });
+            }
+          });
+        });
+      }
+      table.append(tbody);
+
+      var newButton = $('<a class="btn btn-primary" href="#" data-role="new">New</a>');
+      newButton.click(function() {
+        edit(false);
+        return false;
+      });
+
+      container.html('');
+      container.append(newButton);
+      container.append(table);
+    }
+
+    function edit(datum)
+    {
+      var isNew = (datum === false);
+      var outer = $('<div class="editor"></div>');
+      var form = $('<form></form>');
+      if (isNew)
+      {
+        datum = {};
+        eachColumn(function(column) {
+          // If no default is specified for this column, use the default
+          // for the column type (for instance, a nested admin must at least
+          // be an empty array).
+          if (column.defaultValue !== undefined)
+          {
+            datum[column.name] = column.defaultValue;
+          }
+          else
+          {
+            datum[column.name] = options.types[column.type].defaultValue;
+          }
+        });
+      }
+      eachColumn(function(column) {
+        var fieldset = $('<fieldset data-role="fieldset"><label data-role="label"></label></fieldset>');
+        var arrow = $('<i class="attention-arrow icon-hand-right"></i>');
+        arrow.hide();
+        fieldset.append(arrow);
+        column.arrow = arrow;
+        fieldset.find('[data-role="label"]').text(column.label);
+        var control = options.types[column.type].control(column, datum[column.name]);
+        control.attr('data-column', column.name);
+        fieldset.append(control);
+        form.append(fieldset);
+      });
+      outer.append(form);
+      var saveButton = $('<a class="btn btn-primary" href="#" data-role="submit">Save</a>');
+      var cancelButton = $('<a class="btn" href="#" data-role="cancel">Cancel</a>');
+      var removeButton;
+      if (!isNew)
+      {
+        var removeButton = $('<a class="btn btn-warning" href="#" data-role="delete">Delete</a>');
+      }
+      saveButton.click(function() {
+        var invalid = false;
+        eachColumn(function(column) {
+          // Some types just update the datum directly
+          if (!options.types[column.type].selfUpdating)
+          {
+            datum[column.name] = form.find('[data-column="' + column.name + '"]').val();
+          }
+          if (column.required && (!datum[column.name]))
+          {
+            column.arrow.show();
+            // Flag missing items
+            invalid = true;
+          }
+          if (column.unique && (isDuplicate(datum, column)))
+          {
+            column.arrow.show();
+            // Flag duplicate items
+            invalid = true;
+          }
+        });
+        if (invalid)
+        {
+          return false;
+        }
+        var method = isNew ? 'create' : 'update';
+        options[method](
+          datum, 
+          { 
+            'success': function(newDatum) {
+              // We have consistently been burned when we have
+              // assumed we know exactly how to update the list view
+              // from the browser side. Think about pagination, 
+              // sort order, and server side cleaning of the data. 
+              // Discover humility. Call refresh. Om.
+              options.refreshData({ success: list });
+            }
+          }
+        );
+        return false;
+      });
+      cancelButton.click(function() {
+        list();
+        return false;
+      });
+      if (!isNew)
+      {
+        removeButton.click(function() {
+          if (!options.removeConfirm)
+          {
+            options.removeConfirm = "Are you sure you want to delete this item?";
+          }
+          if (confirm(options.removeConfirm))
+          {
+            options.remove(
+              datum,
+              {
+                'success':  function(datum) {
+                  // We have consistently been burned when we have
+                  // assumed we know exactly how to update the list view
+                  // from the browser side. Think about pagination, 
+                  // sort order, and server side cleaning of the data. 
+                  // Discover humility. Call refresh. Om.
+                  options.refreshData({ success: list });
+                }
+              }
+            );
+          }
+          return false;
+        });
+      }
+      outer.append(saveButton);
+      outer.append(cancelButton);
+      if (!isNew)
+      {
+        outer.append(removeButton);
+      }
+      container.html('');
+      container.append(outer);
+    }
+
+    function isDuplicate(datum, column)
+    {
+      return _.any(options.data, function(item) {
+        return ((datum[column.name] === item[column.name]) && (datum[idColumn] !== item[idColumn]));
+      });
+    }
+  };
+
+})( jQuery );
